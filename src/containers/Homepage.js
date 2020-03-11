a7ff47443d80860f2f5dd489d2233b6cc7dc0aa8
stvik <stvik2@gmail.com>
2020-03-11 16:50:02 -0400

    styling

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,14 +1,21 @@
 import React, { Fragment } from 'react';
 import { Header, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 
 function Homepage() {
   return (
   	<div className='headerContainer'>
-  		<Header size='huge' inverted className='nameHeader'>Sara Tarnvik</Header>
-  		<div>
-  			<Icon circular name='github' size='big'/>
-  			<Icon circular name='linkedin' size='big'/>
+  		<Header inverted className='nameHeader'>Sara Tarnvik</Header>
+  		<Header size='huge' color='grey' inverted>Full-Stack Software Engineer</Header>
+
+  		<div  style={{paddingTop:'2vh'}}>
+  			<a href='https://github.com/stvik' target='_blank' style={{paddingRight:'2vw'}}>
+  				<Icon circular inverted link name='github' size='big'/>
+  			</a>
+  			<a href='https://www.linkedin.com/in/sara-tarnvik/' target='_blank'>
+  				<Icon circular link inverted name='linkedin' size='big'/>
+  			</a>
   		</div>
   	</div>
 
