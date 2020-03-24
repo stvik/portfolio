4bd2721b2afe25ee5b3ce0cdb1e6c50e34e58040
stvik <stvik2@gmail.com>
2020-03-24 15:34:36 -0400

    added aboutme

--- src/containers/Aboutpage.js
+++ src/containers/Aboutpage.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { Image, Header } from 'semantic-ui-react'
+import { Image, Header, Container } from 'semantic-ui-react'
 
 
 function Aboutpage() {
@@ -7,7 +7,10 @@ function Aboutpage() {
     <>
     	<Image src='https://miro.medium.com/max/3150/2*UAedSBzj368D-b3Nw5UpNw.jpeg' circular className='profileImage' centered/>
     	<Header as='h1' color='grey' textAlign='center'>Hello, I'm Sara</Header>
-    	
+    	<Container>
+	    	<Header as='h3' textAlign='center'>I am a full-stack software engineer experienced in JavaScript, React, and Ruby on Rails, with a background in software application support, customer service and analytics. 
+	    	Personable and reliable, with excellent team management and customer interaction skills. Prior education in industrial and systems engineering with a focus on sustainability. </Header>
+	    </Container>
     </>
   )
 }
